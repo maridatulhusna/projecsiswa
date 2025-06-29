1d0b6ced7ef72ef9f0b444e145a825c04a807e82
maridatulhusna <maridatulhusna@gmail.com>
2025-06-29 14:15:50 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Beranda - Sistem Informasi</title>
+    <style>
+        body {
+            background-image: url('gambar.jpeg'); 
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+            font-family: Arial, sans-serif;
+            color: white;
+            text-align: center;
+            padding-top: 100px;
+        }
+
+        h2 {
+            background-color: rgba(0, 0, 0, 0.6);
+            display: inline-block;
+            padding: 10px 20px;
+            border-radius: 10px;
+        }
+
+        ul {
+            list-style: none;
+            padding: 0;
+            margin-top: 30px;
+        }
+
+        li {
+            margin: 10px;
+        }
+
+        a {
+            background-color: #ffffffaa;
+            color: #000;
+            padding: 10px 20px;
+            text-decoration: none;
+            border-radius: 8px;
+            font-weight: bold;
+            transition: 0.3s;
+        }
+
+        a:hover {
+            background-color: #00bcd4;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <h2>Selamat Datang di Sistem Informasi data Siswa,Kelas & Guru SMK Ibrahimy 1</h2>
+    <ul>
+        <li><a href="kelas/"> Data Kelas</a></li>
+        <li><a href="siswa/">Data Siswa</a></li>
+         <li><a href="Guru/"> Data Guru</a></li>
+    </ul>
+</body>
+</html>
